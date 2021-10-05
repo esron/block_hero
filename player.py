3d7627a585ddc28675a343e379cb1f44b1cd949e
Esron Silva <esron.dtamar@gmail.com>
2021-10-05 20:45:16 -0300

    Add Destiny logic

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import ppb
-from ppb.buttons import Primary
-from ppb.events import ButtonPressed, ButtonReleased, MouseMotion
+from ppb.events import ButtonPressed, ButtonReleased, MouseMotion, Update
 from ppb_vector import Vector
+from destiny import Destiny
 
 
 class Player(ppb.Sprite):
@@ -10,8 +10,14 @@ class Player(ppb.Sprite):
     speed  = 4
     target = position
 
-    def on_update(self, update_event, signal):
+    def on_update(self, update_event: Update, signal):
         self.position += self.direction * self.speed * update_event.time_delta
+
+        for d in update_event.scene.get(kind=Destiny):
+            if (d.position - self.position).length <= d.size:
+                update_event.scene.remove(d)
+                break
+
         if self.target - self.position != Vector(0, 0): #  Prevent division by zero
             self.direction = (self.target - self.position).normalize()
             return
@@ -23,6 +29,7 @@ class Player(ppb.Sprite):
         self.speed = 4
 
     def on_button_released(self, button_event: ButtonReleased, signal):
+        button_event.scene.add(Destiny(button_event.position))
         self.target = button_event.position
         self.speed = 4
 
