1b2484e6843628cd88383a3c74cb744d5b56b100
Esron Silva <esron.dtamar@gmail.com>
2021-10-05 20:45:34 -0300

    Typehints Update event

--- target.py
+++ target.py
@@ -1,11 +1,11 @@
 import ppb
-
+from ppb.events import Update
 from projectile import Projectile
 
 
 class Target(ppb.Sprite):
 
-    def on_update(self, update_event, signal):
+    def on_update(self, update_event: Update, signal):
         for p in update_event.scene.get(kind=Projectile):
             if (p.position - self.position).length <= self.size:
                 update_event.scene.remove(self)
