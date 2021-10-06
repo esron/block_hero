21924d42f8df5405f2cad8217afd0abadafd8ae1
Esron Silva <esron.dtamar@gmail.com>
2021-10-05 22:58:39 -0300

    Fix fliquering effect

--- player.py
+++ player.py
@@ -7,22 +7,22 @@ from destiny import Destiny
 class Player(ppb.Sprite):
     position = ppb.Vector(0, -7)
     direction = ppb.Vector(0, 0)
-    speed  = 4
-    target = position
+    speed  = 0
+    target = Vector(0 ,0)
 
     def on_update(self, update_event: Update, signal):
         self.position += self.direction * self.speed * update_event.time_delta
 
-        for d in update_event.scene.get(kind=Destiny):
-            if (d.position - self.position).length <= d.size:
-                update_event.scene.remove(d)
-                break
-
-        if self.target - self.position != Vector(0, 0): #  Prevent division by zero
+        if (self.target - self.position).length > 0.0001: #  Prevent division by zero
             self.direction = (self.target - self.position).normalize()
-            return
+        else:
+            self.speed = 0
 
-        self.speed = 0
+        for d in update_event.scene.get(kind=Destiny):
+            if (d.position - self.position).length + 0.4 < d.size:
+                update_event.scene.remove(d)
+                self.speed = 0
+                break
 
     def on_button_pressed(self, button_event: ButtonPressed, signal):
         self.target = button_event.position
