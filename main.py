ff45c4d9a658d340edd536ffce285af76b7818f8
Esron Silva <esron.dtamar@gmail.com>
2021-10-09 20:38:52 -0300

    Add spawner to main scene

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import ppb
 from player import Player
+from spawner import Spawner
 from target import Target
 
 
 def setup(scene):
     scene.add(Player())
+    scene.add(Spawner())
 
     for x in range(-4, 5, 2):
         scene.add(Target(position=ppb.Vector(x, 3)))
