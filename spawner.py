e3ce40f1eb5ac1a376609e18bd2904ae7bc8e551
Esron Silva <esron.dtamar@gmail.com>
2021-10-09 20:38:26 -0300

    Add a target spawner

---
+++ spawner.py
@@ -0,0 +1,28 @@
+import ppb
+from ppb.events import Update
+from ppb_vector import Vector
+from player import Player
+from target import Target
+
+class Spawner(ppb.Sprite):
+    size = 3
+    position = Vector(0, 8)
+    max_enemies = 4
+    spawn_timer = 0
+    spawn_seconds = 2
+
+    def on_update(self, update_event: Update, signal):
+        self.spawn_timer += update_event.time_delta
+
+        current_enemies = len(tuple(update_event.scene.get(kind=Target)))
+
+        if current_enemies < self.max_enemies and self.spawn_timer > self.spawn_seconds:
+
+            update_event.scene.add(Target(position=self.position))
+            self.spawn_timer = 0
+
+        if current_enemies != 0:
+            for p in update_event.scene.get(kind=Player):
+                for t in update_event.scene.get(kind=Target):
+                    t.direction = (p.position - t.position).normalize()
+                break
