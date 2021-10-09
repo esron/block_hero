05aaea6b82a30cf663eb3f45c76e3a5d1364dcfd
Esron Silva <esron.dtamar@gmail.com>
2021-10-09 20:38:37 -0300

    Make target move

--- target.py
+++ target.py
@@ -1,9 +1,12 @@
 import ppb
 from ppb.events import Update
+from ppb_vector import Vector
 from projectile import Projectile
 
 
 class Target(ppb.Sprite):
+    speed = 2
+    direction = Vector(0, 0)
 
     def on_update(self, update_event: Update, signal):
         for p in update_event.scene.get(kind=Projectile):
@@ -11,3 +14,5 @@ class Target(ppb.Sprite):
                 update_event.scene.remove(self)
                 update_event.scene.remove(p)
                 break
+
+        self.position += self.direction * self.speed * update_event.time_delta
