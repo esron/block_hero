6b287dfcd60fa7ebf1c94686567d57175ba5e8be
Esron Silva <esron.dtamar@gmail.com>
2021-10-09 21:31:45 -0300

    Add Score to scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import ppb
 from player import Player
+from score import Score
 from spawner import Spawner
 from target import Target
 
@@ -7,6 +8,7 @@ from target import Target
 def setup(scene):
     scene.add(Player())
     scene.add(Spawner())
+    scene.add(Score())
 
     for x in range(-4, 5, 2):
         scene.add(Target(position=ppb.Vector(x, 3)))
