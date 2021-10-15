dfad372a1ada38c0ceba194c2c0d94d6fea5f873
Esron Silva <esron.dtamar@gmail.com>
2021-10-15 18:51:14 -0300

    Improve score Resolution

--- score.py
+++ score.py
@@ -6,13 +6,13 @@ class Score(ppb.Sprite):
     points = 0
     image = ppb.Text(
         f'SCORE  {points}',
-        font=ppb.Font('fonts\RobotoMono-Regular.ttf', size=12),
+        font=ppb.Font('fonts\RobotoMono-Regular.ttf', size=24),
         color=(255, 255, 255)
     )
 
     def on_update(self, update_event: Update, signal):
         self.image = ppb.Text(
             f'SCORE  {self.points}',
-            font=ppb.Font('fonts\RobotoMono-Regular.ttf', size=12),
+            font=ppb.Font('fonts\RobotoMono-Regular.ttf', size=24),
             color=(255, 255, 255)
         )
