771e20892d183b37fe417e2d7124a88a0f7d07cc
Esron Silva <esron.dtamar@gmail.com>
2021-10-16 17:54:59 -0300

    Add a game over screen

---
+++ game_over_screen.py
@@ -0,0 +1,12 @@
+import ppb
+from ppb.events import KeyPressed, StopScene
+
+class GameOverScreen(ppb.Sprite):
+    image = ppb.Text(
+        f'GAME OVER\n\nPRESS ANY KEY',
+        font=ppb.Font('fonts\RobotoMono-Regular.ttf', size=48),
+        color=(255, 255, 255)
+    )
+
+    def on_key_pressed(self, key_event: KeyPressed, signal):
+        signal(StopScene(key_event.scene))
