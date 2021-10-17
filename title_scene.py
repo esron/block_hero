1f9526898bb62778f24c025cdd8252f8ce47585f
Esron Silva <esron.dtamar@gmail.com>
2021-10-17 10:51:50 -0300

    Use GameScene

--- title_scene.py
+++ title_scene.py
@@ -1,9 +1,6 @@
 import ppb
 from ppb.events import KeyPressed, SceneStarted, StartScene
-from player import Player
-from score import Score
-from spawner import Spawner
-from target import Target
+from game_scene import GameScene
 from title_sprite import TitleSprite
 
 
@@ -14,12 +11,4 @@ class TitleScene(ppb.Scene):
         self.add(TitleSprite())
 
     def on_key_pressed(self, key_event: KeyPressed, signal):
-        game_scene = ppb.Scene()
-        game_scene.add(Player())
-        game_scene.add(Spawner())
-        game_scene.add(Score())
-
-        # Add 5 initial targets
-        for x in range(-4, 5, 2):
-            game_scene.add(Target(position=ppb.Vector(x, 3)))
-        signal(StartScene(game_scene))
+        signal(StartScene(GameScene()))
