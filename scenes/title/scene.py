e73b98319347117020eb04f360fef074978ddccc
Esron Silva <esron.dtamar@gmail.com>
2021-10-17 18:12:56 -0300

    Refactor Title Scene to use new sprites

--- scenes/title/scene.py
+++ scenes/title/scene.py
@@ -1,6 +1,12 @@
 import ppb
-from ppb.events import KeyPressed, SceneStarted, StartScene
+from ppb.events import KeyPressed, Quit, SceneStarted, StartScene
+from ppb.keycodes import Q, Space
 from scenes.game.scene import GameScene
+from scenes.title.sprites.quit_sprite import QuitSprite
+from scenes.title.sprites.rule_one_sprite import RuleOneSprite
+from scenes.title.sprites.rule_two_sprite import RuleTwoSprite
+from scenes.title.sprites.rules_sprite import RulesSprite
+from scenes.title.sprites.start_sprite import StartSprite
 from scenes.title.sprites.title_sprite import TitleSprite
 
 
@@ -9,6 +15,16 @@ class TitleScene(ppb.Scene):
 
     def on_scene_started(self, scene_event: SceneStarted, signal):
         self.add(TitleSprite())
+        self.add(RulesSprite())
+        self.add(RuleOneSprite())
+        self.add(RuleTwoSprite())
+        self.add(StartSprite())
+        self.add(QuitSprite())
 
     def on_key_pressed(self, key_event: KeyPressed, signal):
-        signal(StartScene(GameScene()))
+        if key_event.key == Q:
+            signal(Quit())
+            return
+
+        if key_event.key == Space:
+            signal(StartScene(GameScene()))
