b72369947747a090217b61fda2df3231539633c1
Esron Silva <esron.dtamar@gmail.com>
2021-10-17 11:12:03 -0300

    Move stop scene to game over scene

---
+++ game_over_scene.py
@@ -0,0 +1,13 @@
+import ppb
+from ppb.events import KeyPressed, SceneStarted, StopScene
+
+from game_over_sprite import GameOverSprite
+
+class GameOverScene(ppb.Scene):
+    background_color = (0, 0, 0)
+
+    def on_scene_started(self, scene_event: SceneStarted, signal):
+        self.add(GameOverSprite())
+
+    def on_key_pressed(self, key_event: KeyPressed, signal):
+        signal(StopScene(key_event.scene))
--- game_over_sprite.py
+++ game_over_sprite.py
@@ -1,5 +1,4 @@
 import ppb
-from ppb.events import KeyPressed, StopScene
 
 class GameOverSprite(ppb.Sprite):
     image = ppb.Text(
@@ -7,6 +6,3 @@ class GameOverSprite(ppb.Sprite):
         font=ppb.Font('fonts\RobotoMono-Regular.ttf', size=48),
         color=(255, 255, 255)
     )
-
-    def on_key_pressed(self, key_event: KeyPressed, signal):
-        signal(StopScene(key_event.scene))
