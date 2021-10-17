c0502014c720983677be739a7849e6db9a596586
Esron Silva <esron.dtamar@gmail.com>
2021-10-17 17:21:49 -0300

    Adjust title sprite

--- scenes/title/sprites/title_sprite.py
+++ scenes/title/sprites/title_sprite.py
@@ -1,9 +1,12 @@
 import ppb
+from ppb_vector import Vector
 
 
 class TitleSprite(ppb.Sprite):
+    size = 3
+    position = Vector(0, 4)
     image = ppb.Text(
-        'BLOCK HERO\n\nPRESS ANY KEY',
-        font=ppb.Font('fonts\RobotoMono-Regular.ttf', size=48),  # noqa: W605
+        'BLOCK HERO',
+        font=ppb.Font('fonts\RobotoMono-Regular.ttf', size=24*3),  # noqa: W605
         color=(255, 255, 255)
     )
