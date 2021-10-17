57d39c6f35ad6432aac95e3efeb1e7c114441b2a
Esron Silva <esron.dtamar@gmail.com>
2021-10-17 18:03:51 -0300

    Add Rules Sprite

---
+++ scenes/title/sprites/rules_sprite.py
@@ -0,0 +1,11 @@
+import ppb
+from ppb_vector import Vector
+
+
+class RulesSprite(ppb.Sprite):
+    position = Vector(0, 2)
+    image = ppb.Text(
+        'RULES',
+        font=ppb.Font('fonts\RobotoMono-Regular.ttf', size=24),  # noqa: W605
+        color=(255, 255, 255)
+    )
