9025865966f7021ef4a0616b83137e262bc8a473
Esron Silva <esron.dtamar@gmail.com>
2021-10-17 18:04:26 -0300

    Add rule two sprite

---
+++ scenes/title/sprites/rule_two_sprite.py
@@ -0,0 +1,11 @@
+import ppb
+from ppb_vector import Vector
+
+
+class RuleTwoSprite(ppb.Sprite):
+    position = Vector(0, 0)
+    image = ppb.Text(
+        '2. You can only shoot if you are still',
+        font=ppb.Font('fonts\RobotoMono-Regular.ttf', size=24),  # noqa: W605
+        color=(255, 255, 255)
+    )
