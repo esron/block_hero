de7696c6eda5749cf36d84f779db5f6959f0a37b
Esron Silva <esron.dtamar@gmail.com>
2021-10-17 10:31:03 -0300

    Simplify title sprite to have only text

--- title_sprite.py
+++ title_sprite.py
@@ -1,10 +1,7 @@
 import ppb
 from ppb.events import KeyPressed, StartScene
 
-from player import Player
-from score import Score
-from spawner import Spawner
-from target import Target
+
 
 class TitleSprite(ppb.Sprite):
     image = ppb.Text(
@@ -12,14 +9,3 @@ class TitleSprite(ppb.Sprite):
         font=ppb.Font('fonts\RobotoMono-Regular.ttf', size=48),
         color=(255, 255, 255)
     )
-
-    def on_key_pressed(self, key_event: KeyPressed, signal):
-        main_scene = ppb.Scene()
-        main_scene.add(Player())
-        main_scene.add(Spawner())
-        main_scene.add(Score())
-
-        # Add 5 initial targets
-        for x in range(-4, 5, 2):
-            main_scene.add(Target(position=ppb.Vector(x, 3)))
-        signal(StartScene(main_scene))
