7838fbec29c46fd90a78ee7aa425aa43e8bf6dc7
Esron Silva <esron.dtamar@gmail.com>
2021-10-17 13:16:37 -0300

    Implement a Pause function

--- scenes/game/scene.py
+++ scenes/game/scene.py
@@ -9,6 +9,7 @@ from scenes.game_over.scene import GameOverScene
 from scenes.pause.scene import PauseScene
 
 class GameScene(ppb.Scene):
+    paused = False
 
     def on_scene_started(self, scene_event: SceneStarted, signal):
         self.add(Player())
@@ -29,8 +30,14 @@ class GameScene(ppb.Scene):
     # Work around a scene transaction flow from
     # Start Scene -> Game -> Game Over -> Start Scene
     def on_scene_continued(self, scene_event: SceneContinued, signal):
-        signal(StopScene(scene_event.scene))
+        if not self.paused:
+            signal(StopScene(scene_event.scene))
+
+        self.paused = False
+
+
 
     def on_key_pressed(self, key_event: KeyPressed, signal):
         if key_event.key == Escape:
+            self.paused = True
             signal(StartScene(PauseScene()))
