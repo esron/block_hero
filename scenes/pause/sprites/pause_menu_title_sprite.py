e9f880f7dceb0fb20f83af298f9c0e0f3697fe43
Esron Silva <esron.dtamar@gmail.com>
2021-10-19 20:17:57 -0300

    Resise pause menu title

--- scenes/pause/sprites/pause_menu_title_sprite.py
+++ scenes/pause/sprites/pause_menu_title_sprite.py
@@ -1,9 +1,12 @@
 import ppb
+from ppb_vector import Vector
 
 
 class PauseMenuTitleSprite(ppb.Sprite):
+    size = 2
+    position = Vector(0, 2)
     image = ppb.Text(
         'GAME PAUSED',
-        font=ppb.Font('fonts\RobotoMono-Regular.ttf', size=24),  # noqa: W605
+        font=ppb.Font('fonts\RobotoMono-Regular.ttf', size=24*2),  # noqa: W605
         color=(255, 255, 255)
     )
