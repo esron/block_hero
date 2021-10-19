86c9330cf606846edd45a05e0d0f423e49832562
Esron Silva <esron.dtamar@gmail.com>
2021-10-19 20:18:32 -0300

    Use continue sprite

--- scenes/pause/scene.py
+++ scenes/pause/scene.py
@@ -1,5 +1,6 @@
 import ppb
 from ppb.events import KeyPressed, SceneStarted, StopScene
+from scenes.pause.sprites.continue_sprite import ContinueSprite
 
 from scenes.pause.sprites.pause_menu_title_sprite import PauseMenuTitleSprite
 
@@ -9,6 +10,7 @@ class PauseScene(ppb.Scene):
 
     def on_scene_started(self, scene_event: SceneStarted, signal):
         self.add(PauseMenuTitleSprite())
+        self.add(ContinueSprite())
 
     def on_key_pressed(self, key_event: KeyPressed, signal):
         signal(StopScene(key_event.scene))
