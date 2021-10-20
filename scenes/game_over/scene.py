555eaf4c5ea310c116609bcb076988ca64997476
Esron Silva <esron.dtamar@gmail.com>
2021-10-20 19:48:06 -0300

    Add a quit feature to game over scene

--- scenes/game_over/scene.py
+++ scenes/game_over/scene.py
@@ -1,5 +1,8 @@
 import ppb
-from ppb.events import KeyPressed, SceneStarted, StopScene
+from ppb.events import KeyPressed, Quit, SceneStarted, StopScene
+from ppb.keycodes import Q, Space
+from global_sprites.continue_sprite import ContinueSprite
+from global_sprites.quit_sprite import QuitSprite
 
 from scenes.game_over.sprites.game_over_sprite import GameOverSprite
 
@@ -9,6 +12,11 @@ class GameOverScene(ppb.Scene):
 
     def on_scene_started(self, scene_event: SceneStarted, signal):
         self.add(GameOverSprite())
+        self.add(ContinueSprite())
+        self.add(QuitSprite())
 
     def on_key_pressed(self, key_event: KeyPressed, signal):
-        signal(StopScene(key_event.scene))
+        if key_event.key == Space:
+            signal(StopScene(key_event.scene))
+        if key_event.key == Q:
+            signal(Quit())
