5f9d8299f812143be783c246a5a9543895b863af
Esron Silva <esron.dtamar@gmail.com>
2021-10-20 19:44:49 -0300

    Resize game over sprite

--- scenes/game_over/sprites/game_over_sprite.py
+++ scenes/game_over/sprites/game_over_sprite.py
@@ -2,8 +2,9 @@ import ppb
 
 
 class GameOverSprite(ppb.Sprite):
+    size = 3
     image = ppb.Text(
-        'GAME OVER\n\nPRESS ANY KEY',
-        font=ppb.Font('fonts\RobotoMono-Regular.ttf', size=48),  # noqa: W605
+        'GAME OVER',
+        font=ppb.Font('fonts\RobotoMono-Regular.ttf', size=24*3),  # noqa: W605
         color=(255, 255, 255)
     )
